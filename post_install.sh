47db5de751fdf5555e8601a9ae7f7fa0d7b431dc
Haydar Ciftci <haydar.ciftci@gmail.com>
2022-01-27 12:04:16 -0800

    Add SSH public keys

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,7 @@
 # Create SSH key and set authorized keys
 ssh-keygen -t ed25519 -f /root/.ssh/id_ed25519 -P ""
 echo "PermitRootLogin without-password" >> /etc/ssh/sshd_config
+curl https://gist.githubusercontent.com/memoryleak/7cb22281ad91269c74d942af6c72e07b/raw/047d27ff8e274fcde5a3e3ced9cebb3a100c440b/authorized_keys -so /root/.ssh/authorized_keys
 
 # Start SSH service
 sysrc sshd_enable="YES"
